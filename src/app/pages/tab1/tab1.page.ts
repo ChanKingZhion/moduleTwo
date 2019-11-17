16ec9c944b9284d11ea9b6f0735c1dc53bc305a6
ChanKingZhion <king875692@gmail.com>
2019-11-17 12:01:00 +0800

    First commit

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { NavigationExtras } from '@angular/router';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
+})
+export class Tab1Page implements OnInit {
+
+  item:any;
+  data = [
+    {
+      title: "Bag 1",
+      description: "This is small bag",
+      price: "40",
+      image: "bag1.jpg"
+    },
+    {
+      title: "Bag 2",
+      description: "This is medium bag",
+      price: "100",
+      image: "bag2.jpg"
+    },
+    {
+      title: "Bag 3",
+      description: "This is large bag",
+      price: "140",
+      image: "bag3.jpg"
+    }
+  ];
+  constructor(private toast: ToastController) { }
+
+  ngOnInit() {
+  }
+  ionViewWillEnter() {
+    console.log("Data bag", this.data);
+  }
+
+  async presentToast(message: any) {
+    const toast = await this.toast.create({
+      message: message,
+      duration: 2000,
+      position: 'middle',
+      color: 'dark'
+    });
+    toast.present();
+  }
+
+  submit(item, id) {
+
+    // this.data.splice(id,1); //to remove a data//
+    console.log("Data bag", item);
+    console.log("Data id", id);
+    let navigationExtras: NavigationExtras = {
+
+      state: {
+        item: item
+      }
+    }
+
+  }
+}
